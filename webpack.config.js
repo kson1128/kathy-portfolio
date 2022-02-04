f37bf967ed33fe9dddc5e20a3af688e9ef5d6389
kson1128 <69124193+kson1128@users.noreply.github.com>
2022-02-04 18:22:07 -0500

    add: dependencies, skeloton, webpack config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'development',
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: './bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
