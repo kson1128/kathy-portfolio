90f917a0d15e05c6d01da6b3c3e2e7d3b7cb801d
kson1128 <69124193+kson1128@users.noreply.github.com>
2022-06-24 03:24:30 -0400

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const dotenv = require('dotenv');
 
 const env = dotenv.config().parsed;
-const envKeys = Object.keys(env).reduce((prev, next) => {
+const envKeys = Object.keys(null || env).reduce((prev, next) => {
   prev[`process.env.${next}`] = JSON.stringify(env[next]);
   return prev;
 }, {});
