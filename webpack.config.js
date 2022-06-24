193fbf596936dbc19ca92502eca34b42669c0bf8
kson1128 <69124193+kson1128@users.noreply.github.com>
2022-06-24 02:13:24 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const envKeys = Object.keys(env).reduce((prev, next) => {
 module.exports = {
   plugins: [new webpack.DefinePlugin(envKeys)],
 
-  mode: 'development',
+  mode: 'production',
   entry: './index.js',
   output: {
     path: __dirname,
