20cdf7ef91e399b2c91619491386570b03519214
kson1128 <69124193+kson1128@users.noreply.github.com>
2022-06-24 01:41:03 -0400

    change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,14 +9,7 @@ const envKeys = Object.keys(env).reduce((prev, next) => {
 }, {});
 module.exports = {
   plugins: [new webpack.DefinePlugin(envKeys)],
-  //   new webpack.DefinePlugin({
-  //     'process.env': JSON.stringify(dotenv.parsed),
-  //     'process.env.NODE_ENV': JSON.stringify(
-  //       isDevelopment ? 'development' : 'production'
-  //     ),
-  //   }),
-  // ].filter(Boolean),
-  mode: 'development',
+  mode: 'production',
   entry: './index.js',
   output: {
     path: __dirname,
@@ -29,7 +22,7 @@ module.exports = {
     // host: '0.0.0.0',
     port: 8080, // port for dev server
   },
-  watch: process.env.NODE_ENV === 'development',
+  watch: process.env.NODE_ENV === 'production',
   module: {
     rules: [
       {
