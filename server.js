f24f54849e7cf4404b35b190ec466c389539f324
Kathy Son <69124193+kson1128@users.noreply.github.com>
2022-06-24 02:17:54 -0400

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-// console.log('No value for FOO yet:', process.env.FOO);
-
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config();
-
-// }
-
-// console.log('Now the value for FOO is:', process.env.FOO);
-console.log('The value for FOO is:', process.env.FOO);
